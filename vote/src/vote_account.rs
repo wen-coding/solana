d3188be4e924ac7832710dbb0fe428f3a909784f
Wen <113942165+wen-coding@users.noreply.github.com>
2025-10-09 08:52:31 -0700

    Add a test for filtering out zero staked nodes.

--- vote/src/vote_account.rs
+++ vote/src/vote_account.rs
@@ -1228,4 +1228,23 @@ mod tests {
             identity_accounts.into_iter().collect::<HashSet<&Pubkey>>()
         );
     }
+
+    #[test]
+    fn test_identity_accounts_for_staked_nodes_remove_zero_stake() {
+        solana_logger::setup();
+        let mut rng = rand::thread_rng();
+        let num_nodes = 10;
+        // Make sure our vote account to identity mapping is 1:1.
+        let vote_accounts = (0..num_nodes)
+            .map(|_| {
+                let account = new_rand_vote_account(&mut rng, None, true);
+                let vote_account = VoteAccount::try_from(account).unwrap();
+                let stake = rng.gen_range(0..MAX_STAKE_FOR_STAKED_ACCOUNT);
+                (Pubkey::new_unique(), (stake, vote_account))
+            })
+            .collect::<VoteAccounts>();
+        let num_staked_accounts = vote_accounts.staked_nodes().len();
+        let identity_accounts = vote_accounts.identity_accounts_for_staked_nodes();
+        assert_eq!(identity_accounts.len(), num_staked_accounts);
+    }
 }
