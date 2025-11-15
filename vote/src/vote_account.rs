f773c6b0b13bd4abc8fd163c76737d18461e918e
Kamil Skalski <kamil.skalski@gmail.com>
2025-11-15 17:45:43 +0800

    chore(vote): switch to rand=0.9.2 (#9071)

--- vote/src/vote_account.rs
+++ vote/src/vote_account.rs
@@ -186,25 +186,25 @@ impl VoteAccount {
             solana_vote_interface::state::{VoteInit, VoteStateV4, VoteStateVersions},
         };
 
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let vote_pubkey = Pubkey::new_unique();
 
         let vote_init = VoteInit {
             node_pubkey: Pubkey::new_unique(),
             authorized_voter: Pubkey::new_unique(),
             authorized_withdrawer: Pubkey::new_unique(),
-            commission: rng.gen(),
+            commission: rng.random(),
         };
         let clock = Clock {
-            slot: rng.gen(),
-            epoch_start_timestamp: rng.gen(),
-            epoch: rng.gen(),
-            leader_schedule_epoch: rng.gen(),
-            unix_timestamp: rng.gen(),
+            slot: rng.random(),
+            epoch_start_timestamp: rng.random(),
+            epoch: rng.random(),
+            leader_schedule_epoch: rng.random(),
+            unix_timestamp: rng.random(),
         };
         let vote_state = VoteStateV4::new(&vote_pubkey, &vote_init, &clock);
         let account = AccountSharedData::new_data(
-            rng.gen(), // lamports
+            rng.random(), // lamports
             &VoteStateVersions::new_v4(vote_state.clone()),
             &solana_sdk_ids::vote::id(), // owner
         )
@@ -566,21 +566,21 @@ mod tests {
             node_pubkey: node_pubkey.unwrap_or_else(Pubkey::new_unique),
             authorized_voter: Pubkey::new_unique(),
             authorized_withdrawer: Pubkey::new_unique(),
-            commission: rng.gen(),
+            commission: rng.random(),
         };
         let clock = Clock {
-            slot: rng.gen(),
-            epoch_start_timestamp: rng.gen(),
-            epoch: rng.gen(),
-            leader_schedule_epoch: rng.gen(),
-            unix_timestamp: rng.gen(),
+            slot: rng.random(),
+            epoch_start_timestamp: rng.random(),
+            epoch: rng.random(),
+            leader_schedule_epoch: rng.random(),
+            unix_timestamp: rng.random(),
         };
         let mut vote_state = VoteStateV4::new(&vote_pubkey, &vote_init, &clock);
         if fill_bls_pubkey {
             vote_state.bls_pubkey_compressed = Some([42; BLS_PUBLIC_KEY_COMPRESSED_SIZE]);
         }
         AccountSharedData::new_data(
-            rng.gen(), // lamports
+            rng.random(), // lamports
             &VoteStateVersions::new_v4(vote_state.clone()),
             &solana_sdk_ids::vote::id(), // owner
         )
@@ -600,9 +600,10 @@ mod tests {
     ) -> impl Iterator<Item = (Pubkey, (/*stake:*/ u64, VoteAccount))> + '_ {
         let nodes: Vec<_> = repeat_with(Pubkey::new_unique).take(num_nodes).collect();
         repeat_with(move || {
-            let node = nodes[rng.gen_range(0..nodes.len())];
+            let node = nodes[rng.random_range(0..nodes.len())];
             let account = new_rand_vote_account(rng, Some(node));
             let stake = rng.gen_range(0..MAX_STAKE_FOR_STAKED_ACCOUNT);
+            let stake = rng.random_range(0..997);
             let vote_account = VoteAccount::try_from(account).unwrap();
             (Pubkey::new_unique(), (stake, vote_account))
         })
@@ -654,7 +655,7 @@ mod tests {
 
     #[test]
     fn test_vote_account_try_from() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let account = new_rand_vote_account(&mut rng, None);
         let lamports = account.lamports();
         let vote_account = VoteAccount::try_from(account.clone()).unwrap();
@@ -665,7 +666,7 @@ mod tests {
     #[test]
     #[should_panic(expected = "InvalidOwner")]
     fn test_vote_account_try_from_invalid_owner() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let mut account = new_rand_vote_account(&mut rng, None);
         account.set_owner(Pubkey::new_unique());
         VoteAccount::try_from(account).unwrap();
@@ -681,7 +682,7 @@ mod tests {
 
     #[test]
     fn test_vote_account_serialize() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let account = new_rand_vote_account(&mut rng, None);
         let vote_account = VoteAccount::try_from(account.clone()).unwrap();
         // Assert that VoteAccount has the same wire format as Account.
@@ -693,7 +694,7 @@ mod tests {
 
     #[test]
     fn test_vote_accounts_serialize() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let vote_accounts_hash_map: VoteAccountsHashMap =
             new_rand_vote_accounts(&mut rng, 64).take(1024).collect();
         let vote_accounts = VoteAccounts::from(Arc::new(vote_accounts_hash_map.clone()));
@@ -712,7 +713,7 @@ mod tests {
 
     #[test]
     fn test_vote_accounts_deserialize() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let vote_accounts_hash_map: VoteAccountsHashMap =
             new_rand_vote_accounts(&mut rng, 64).take(1024).collect();
         let data = bincode::serialize(&vote_accounts_hash_map).unwrap();
@@ -728,7 +729,7 @@ mod tests {
 
     #[test]
     fn test_vote_accounts_deserialize_invalid_account() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         // we'll populate the map with 1 valid and 2 invalid accounts, then ensure that we only get
         // the valid one after deserialiation
         let mut vote_accounts_hash_map = HashMap::<Pubkey, (u64, AccountSharedData)>::new();
@@ -761,7 +762,7 @@ mod tests {
 
     #[test]
     fn test_staked_nodes() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let mut accounts: Vec<_> = new_rand_vote_accounts(&mut rng, 64).take(1024).collect();
         let mut vote_accounts = VoteAccounts::default();
         // Add vote accounts.
@@ -776,7 +777,7 @@ mod tests {
         }
         // Remove some of the vote accounts.
         for k in 0..256 {
-            let index = rng.gen_range(0..accounts.len());
+            let index = rng.random_range(0..accounts.len());
             let (pubkey, (_, _)) = accounts.swap_remove(index);
             vote_accounts.remove(&pubkey);
             if (k + 1) % 32 == 0 {
@@ -785,9 +786,9 @@ mod tests {
         }
         // Modify the stakes for some of the accounts.
         for k in 0..2048 {
-            let index = rng.gen_range(0..accounts.len());
+            let index = rng.random_range(0..accounts.len());
             let (pubkey, (stake, _)) = &mut accounts[index];
-            let new_stake = rng.gen_range(0..MAX_STAKE_FOR_STAKED_ACCOUNT);
+            let new_stake = rng.random_range(0..997);
             if new_stake < *stake {
                 vote_accounts.sub_stake(pubkey, *stake - new_stake);
             } else {
@@ -800,7 +801,7 @@ mod tests {
         }
         // Remove everything.
         while !accounts.is_empty() {
-            let index = rng.gen_range(0..accounts.len());
+            let index = rng.random_range(0..accounts.len());
             let (pubkey, (_, _)) = accounts.swap_remove(index);
             vote_accounts.remove(&pubkey);
             if accounts.len() % 32 == 0 {
@@ -814,7 +815,7 @@ mod tests {
     fn test_staked_nodes_update() {
         let mut vote_accounts = VoteAccounts::default();
 
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let pubkey = Pubkey::new_unique();
         let node_pubkey = Pubkey::new_unique();
         let account1 = new_rand_vote_account(&mut rng, Some(node_pubkey));
@@ -867,7 +868,7 @@ mod tests {
     fn test_staked_nodes_zero_stake() {
         let mut vote_accounts = VoteAccounts::default();
 
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let pubkey = Pubkey::new_unique();
         let node_pubkey = Pubkey::new_unique();
         let account1 = new_rand_vote_account(&mut rng, Some(node_pubkey));
@@ -897,7 +898,7 @@ mod tests {
     // Asserts that returned staked-nodes are copy-on-write references.
     #[test]
     fn test_staked_nodes_cow() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let mut accounts = new_rand_vote_accounts(&mut rng, 64);
         // Add vote accounts.
         let mut vote_accounts = VoteAccounts::default();
@@ -929,7 +930,7 @@ mod tests {
     // Asserts that returned vote-accounts are copy-on-write references.
     #[test]
     fn test_vote_accounts_cow() {
-        let mut rng = rand::thread_rng();
+        let mut rng = rand::rng();
         let mut accounts = new_rand_vote_accounts(&mut rng, 64);
         // Add vote accounts.
         let mut vote_accounts = VoteAccounts::default();
